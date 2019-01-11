d3d23cbca5260cd58887f59239905cb4ca1fec5d
Raphael Sathler <sather93@gmail.com>
2019-01-11 06:59:51 -0200

    Teste da SWAPI Querier

--- app/tests/routes/test_routes.py
+++ app/tests/routes/test_routes.py
@@ -1,9 +1,26 @@
 """ Module for test the endpoints
 """
-
-from unittest import TestCase
+from unittest import (TestCase, mock)
 
 from app.autoapp import APP
+
+from app.api.utils.swapi_querier import SWAPI
+
+def mocked_requests_get(*args, **kwargs):
+    """This method will be used by the mock to replace requests.get
+    """
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    if args[0] == 'https://swapi.co/api/planets/?search=Tatooine&format=json':
+        return MockResponse({}, 500)
+
+    return MockResponse(None, 404)
 
 class EndpointsResponseStatusCodeTest(TestCase):
     """ Class to test whether the responde status code of endpoints are the desired
@@ -42,3 +59,15 @@ class EndpointsResponseStatusCodeTest(TestCase):
         self.assertEqual(err_msg['err']['msg'],
                          'This route is currently not supported.\
                              Please refer API documentation.')
+
+    @mock.patch('app.api.utils.swapi_querier.requests.get', side_effect=mocked_requests_get)
+    def test_if_get_apparitions_count_raises_value_error_when_request_is_not_200(self, mock_get):
+        """Test if there will raise the value error if 
+        status code is not 200
+        """
+
+        swapi = SWAPI()
+        response = swapi.get_apparitions_count('Tatooine')
+        self.assertRaises(ValueError)
+
+
